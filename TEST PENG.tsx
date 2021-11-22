75e3d0bfbdc8d9585d8c4bb729acd8b91f8b3190
avaxpengfam <avaxpenguinfamily@gmail.com>
2021-11-22 22:10:09 +0100

    Intégration benqi Map

--- TEST PENG.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="TEST PENG" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="src/assets/benqi32x32_2.png" width="32" height="32"/>
-</tileset>
