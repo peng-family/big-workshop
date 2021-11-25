eb77a4e76e1d27b0733d8aa8e2cf9b5cecc5945d
avaxpengfam <93932043+avaxpengfam@users.noreply.github.com>
2021-11-25 19:36:26 +0100

    rely

--- Frame 13.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Frame 13" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="src/assets/Frame 13.png" width="64" height="64"/>
-</tileset>
