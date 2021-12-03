a2afa0e10cf0cad9c51552fa558502a7171620ce
avaxpengfam <93932043+avaxpengfam@users.noreply.github.com>
2021-12-03 22:06:39 +0100

    conference V1

--- modern_conference.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="modern_conference" tilewidth="32" tileheight="32" tilecount="192" columns="16">
- <image source="src/assets/modern_conference.png" width="512" height="384"/>
-</tileset>
