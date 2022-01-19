ae6b185a501e9e42570960a76594dc22253a7b8b
avaxpengfam <93932043+avaxpengfam@users.noreply.github.com>
2022-01-19 13:49:30 +0100

    correct

--- pangolin (2).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="pangolin (2)" tilewidth="32" tileheight="32" tilecount="184" columns="8">
- <image source="src/assets/pangolin (2).png" width="256" height="736"/>
-</tileset>
