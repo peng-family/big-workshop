c2d95f4af2deb54d5b7683d1b3c9874dee7bc018
= <=>
2022-02-02 11:42:00 +0100

    Add totem for prod

--- src/application/init.ts
+++ src/application/init.ts
@@ -5,16 +5,10 @@ import { Player } from "../models/player";
 import { TotemContract } from "../models/items/totem/TotemContract";
 
 export const initializeApp = async () => {
-  const { web3, pengFamilyContractERC720 } = useWeb3();
+  const { web3 } = useWeb3();
   const account = new Account(web3);
   await account.initializeAccount();
-
-  const penguinContract = new PenguinContract(
-    web3,
-    account,
-    pengFamilyContractERC720
-  );
-
+  const penguinContract = new PenguinContract(web3, account);
   const totemContract = new TotemContract(account);
   const player = new Player(penguinContract, totemContract, account);
   await player.initialize();
