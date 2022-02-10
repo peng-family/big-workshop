ca75b805b16a18086560ccba20d8dbe6c6706f24
ThPeng016 <96741411+ThPeng016@users.noreply.github.com>
2022-02-10 17:04:56 +0000

    Deploying to gh-pages from master @ 2f7e3b6ef6a253e76cefe3aeb7f9a147284e7f24 🚀

--- src/application/beta-quest/dialogs/index.ts
+++ src/application/beta-quest/dialogs/index.ts
@@ -3,4 +3,4 @@ import { peng2Dialog } from "./peng2";
 import { peng3Dialog } from "./peng3";
 import { IDialog } from "../dialogModels";
 
-export default [peng1Dialog, peng2Dialog] as IDialog[];
+export default [peng1Dialog, peng2Dialog,peng3Dialog] as IDialog[];
