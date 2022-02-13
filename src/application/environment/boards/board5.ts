c45a4c97d762174e9768c01dea9f7a48b62b6aac
ThPeng016 <96741411+ThPeng016@users.noreply.github.com>
2022-02-13 08:21:43 +0000

    Deploying to gh-pages from master @ efa4843dbe218ae7aadfb3ceb6dcf8808dea1dba 🚀

---
+++ src/application/environment/boards/board5.ts
@@ -0,0 +1,9 @@
+import { IBoard } from "../dialogModels";
+export const board5: IBoard = {
+  layerName: "board5",
+  dialog: {
+    rectangleName: "board5",
+    speech: "Welcome in the DeFi Island, you will find here AVAX DeFi products. \n But first, what is DeFi ?  \n DeFi, short for decentralized finance, is a generic term for peer-to-peer (P2P) financial services on public blockchains",
+    answer: [{ answer: "Beautiful Island", buttonColor: "normal" }],
+  },
+};
---
+++ src/application/environment/boards/board6.ts
@@ -0,0 +1,9 @@
+import { IBoard } from "../dialogModels";
+export const board6: IBoard = {
+  layerName: "board6",
+  dialog: {
+    rectangleName: "board6",
+    speech: "Welcome in the NFT Island, you will find here AVAX NFT products & projects.",
+    answer: [{ answer: "Peng peng !", buttonColor: "normal" }],
+  },
+};
