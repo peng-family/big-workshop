20b2e190bd8cd78a3f2da803ca83f1fe89cae661
Deftunk <martin.lavie.pro@gmail.com>
2022-02-17 19:56:18 +0000

    Deploying to gh-pages from master @ efcc4f5a970da83addecddaf96c39b5ff0436e34 🚀

--- src/application/environment/exit-manager/index.ts
+++ src/application/environment/exit-manager/index.ts
@@ -3,12 +3,10 @@ export const exitManager = () => {
   console.log("exit manager");
   WA.room.onEnterLayer("exit").subscribe(() => {
     WA.nav.goToRoom(
-      "/@/avax-penguin-family/penguinverse/worldofpengs#start-layer-kalaoexit"
+      "/@/avax-penguin-family/penguinverse/worldofpengs#start-layer-apaexit"
     );
   });
   WA.room.onEnterLayer("exit2").subscribe(() => {
-    WA.nav.goToRoom(
-      "/@/avax-penguin-family/penguinverse/worldofpengs#kalaoexit"
-    );
+    WA.nav.goToRoom("/@/avax-penguin-family/penguinverse/worldofpengs#apaexit");
   });
 };
