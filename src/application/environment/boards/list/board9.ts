6bcb2809b1f8aecd2e3cbc46b2fe33365354dddb
avaxpengfam <thibaultm.gl@gmail.com>
2022-02-17 17:28:47 +0100

    add benqi

---
+++ src/application/environment/boards/list/board9.ts
@@ -0,0 +1,9 @@
+import { IBoard } from "../../dialogModels";
+export const board9: IBoard = {
+  layerName: "board9",
+  dialog: {
+    rectangleName: "board9",
+    speech: "BENQI’s Liquid Staking protocol is a liquid staking solution that tokenizes staked AVAX to grant users the ability to utilize the yield-bearing asset within Decentralized Finance applications",
+    answer: [{ answer: "Sooo nice !", buttonColor: "normal" }],
+  },
+};
--- src/application/environment/boards/list/index.ts
+++ src/application/environment/boards/list/index.ts
@@ -7,6 +7,7 @@ import { board5 } from "./board5";
 import { board6 } from "./board6";
 import { board7 } from "./board7";
 import { board8 } from "./board8";
+import { board9 } from "./board9";
 import { IBoard } from "../../dialogModels";
 
 export default [
@@ -19,5 +20,6 @@ export default [
     board6,
     board7,
     board8,
+    board9,
 ] as IBoard[];
 
