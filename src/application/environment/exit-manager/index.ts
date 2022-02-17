63dd0743d206baae3d271443975a648aabc96cc5
Martin Lavie <martin.lavie.pro@gmail.com>
2022-02-17 20:42:41 +0100

    test another way to exit tp

--- src/application/environment/exit-manager/index.ts
+++ src/application/environment/exit-manager/index.ts
@@ -3,7 +3,12 @@ export const exitManager = () => {
   console.log("exit manager");
   WA.room.onEnterLayer("exit").subscribe(() => {
     WA.nav.goToRoom(
-      "/@/avax-penguin-family/penguinverse/worldofpengs#door-opener/kalao"
+      "/@/avax-penguin-family/penguinverse/worldofpengs#start-layer-kalaoexit"
+    );
+  });
+  WA.room.onEnterLayer("exit2").subscribe(() => {
+    WA.nav.goToRoom(
+      "/@/avax-penguin-family/penguinverse/worldofpengs#kalaoexit"
     );
   });
 };
