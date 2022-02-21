b6094d416bff0016b5cc654951f90feb1ff623f3
L0f0oo <97795346+L0f0oo@users.noreply.github.com>
2022-02-21 14:13:33 +0100

    _tsx

--- winter_biome_tileset_by_katanakiri98_dcmuwks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="winter_biome_tileset_by_katanakiri98_dcmuwks" tilewidth="32" tileheight="32" tilecount="488" columns="8">
- <image source="src/assets/winter_biome_tileset_by_katanakiri98_dcmuwks.png" width="256" height="1952"/>
-</tileset>
